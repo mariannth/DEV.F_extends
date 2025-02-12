caa36ca87bd180f995e0dfea37690e7e1e492466
Mariana Torres <marith.3289@gmail.com>
2025-02-11 20:41:08 -0600

    Añadiendo cambios con objetos a la práctica

--- clase_11_02_25/practica.js
+++ clase_11_02_25/practica.js
@@ -48,3 +48,45 @@ while (i < fruta.length) {
 
 // Imprimir el conteo de cada tipo de fruta
 console.log(conteoFruta);
+
+
+let frutita = ['manzana', 'plátano', 'manzana', 'naranja', 'plátano', 'pera', 'manzana'];
+
+// Crear un objeto para almacenar las cantidades de cada tipo de fruta    
+let conteoFrutita = {};
+
+for (let i = 0; i < frutita.length; i++) {
+    let frutita1 = frutita[i]; // Obtener la fruta actual del arreglo
+
+    // Si la fruta ya existe en el objeto, incrementamos el contador    
+    if (conteoFrutita[frutita1]) {
+        conteoFrutita[frutita1]++;
+    } else {
+        // Si la fruta no existe en el objeto, la agregamos con un contador inicial de 1
+        conteoFrutita[frutita1] = 1; 
+    
+    }
+}
+
+// Imprimir el conteo de cada tipo de fruta
+console.log("Coneo de frutas usando for");
+for (let frutita1 in conteoFrutita) {
+    console.log(`La fruta ${frutita1} se repite ${conteoFrutita[frutita1]} veces`);
+}
+
+// Imprimir el conteo de cada tipo de fruta usando while
+console.log("Coneo de frutas usando while");
+let inicia = 0;
+while (inicia < frutita.length) {
+    let frutita1 = frutita[inicia]; // Obtener la fruta actual del arreglo
+    // Si la fruta ya existe en el objeto, incrementamos el contador    
+    if (conteoFrutita[frutita1]) {
+        conteoFrutita[frutita1]++;
+    } else {
+        // Si la fruta no existe en el objeto, la agregamos con un contador inicial de 1
+        conteoFrutita[frutita1] = 1; 
+    }
+    inicia++; // Incrementar el índice para avanzar al siguiente elemento
+}
+
+
