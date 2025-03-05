eb8b056da1e2cc021796cacfbf24b1ad7859ed64
Mariana Torres <marith.3289@gmail.com>
2025-03-04 20:27:44 -0600

    Agregando ejemplo de otro generador

--- clase_25_02_25/generador-passwords.js
+++ clase_25_02_25/generador-passwords.js
@@ -31,3 +31,51 @@ function copiarPassword() {
     document.execCommand("copy");
     alert("Contraseña copiada al portapapeles.");
 }
+
+
+// const rangeContainer = docuemnto.querySelector('.range-container');
+// const inputRange= document.querySelector('input[type="range"]');
+// const lengthCounter = documento.querySelector('h1');
+// const generateBtn = document.querySelector('.generator from button');
+// const passwordHeading = document.querySelector('.password-container h1');
+
+// const handleChange = () => {
+//     lenghtCounter.textContent = inputRange.value;
+// }
+
+// const generatePassword = (passwordLenght) =>{
+//     let result = '';
+//     let characters = '';
+
+
+//     //Obtener las opciones seleccionadas
+//     const useUppercase = document.querySelector('#uppercase').checked;
+//     const useLowercase = document.querySelector('#lowercase').checked;
+//     const useNumeros = document.querySelector('#numbers').checked;
+//     const useSymbols = document.querySelector('#symbols').checked;
+
+//     //Creamos un conjunto de caracteres a usar en la contraseña
+//     if(useUppercase) characters+= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//     if(useLowercase) characters+= 'abcdefghijklmnopqrstuvwxyz';
+//     if(useNumeros) characters+= '0123456789';
+//     if(useSymbols) characters+= '!@#$%^&*()_-+=<>?/{}[]|';
+//     if(characters === ''){
+//         passwordHeading.textContent = 'Selecciona al menos una opción :)';
+//         return '';
+//     }
+
+//     //Generación de la contraseña
+//     for(let i=0; i< passwordLenght; i++){
+//         result += characters.charAt(Math.floor(Math.random() * characters.length));
+//     }
+//     return result;
+// };
+
+// function PrintPassword = (event) =>{
+//     event.preventDefault();
+//     const password = generarPassword(inputRange.value);
+//     if(password){
+//         passwordHeading.textContent = password; //Muestra la contra generada
+//     }
+// };
+
